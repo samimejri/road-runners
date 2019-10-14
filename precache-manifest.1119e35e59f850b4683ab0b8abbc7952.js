a14c2baa6459067609d0940e894cd6fa2c9145a0
MEJRI Sami - EXT <SMEJR050@ClearChannel.com>
2019-10-14 13:49:30 +0200

    Updates

---
+++ precache-manifest.1119e35e59f850b4683ab0b8abbc7952.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4672ab38e5a43dad9b3c74271b8b8a34",
+    "url": "/road-runners/index.html"
+  },
+  {
+    "revision": "b124b77e0165f81b04a1",
+    "url": "/road-runners/static/css/main.c22ddaf0.chunk.css"
+  },
+  {
+    "revision": "053d14efec5b0db3c45b",
+    "url": "/road-runners/static/js/2.def8ebcf.chunk.js"
+  },
+  {
+    "revision": "b124b77e0165f81b04a1",
+    "url": "/road-runners/static/js/main.b9fe6265.chunk.js"
+  },
+  {
+    "revision": "c7c9c5416dd721309a5c",
+    "url": "/road-runners/static/js/runtime-main.2475c278.js"
+  }
+]);
