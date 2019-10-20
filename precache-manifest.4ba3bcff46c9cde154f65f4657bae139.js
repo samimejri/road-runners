26ace08590f6a2ef113dedfc4223afda3eeb9743
Sami Mejri <smejri@outlook.com>
2019-10-20 20:47:52 +0200

    Updates

---
+++ precache-manifest.4ba3bcff46c9cde154f65f4657bae139.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a012f2321b8d98a4644625eb02c5f6d0",
+    "url": "/road-runners/index.html"
+  },
+  {
+    "revision": "b18a076e4fcafca90c22",
+    "url": "/road-runners/static/css/main.4bc5b827.chunk.css"
+  },
+  {
+    "revision": "dde1bab897a51eb3ae03",
+    "url": "/road-runners/static/js/2.ab592427.chunk.js"
+  },
+  {
+    "revision": "b18a076e4fcafca90c22",
+    "url": "/road-runners/static/js/main.ba0b1644.chunk.js"
+  },
+  {
+    "revision": "c7c9c5416dd721309a5c",
+    "url": "/road-runners/static/js/runtime-main.2475c278.js"
+  },
+  {
+    "revision": "0a3e8c359649ddbfca25f3fd0bb7853b",
+    "url": "/road-runners/static/media/foreigner-webfont.0a3e8c35.woff"
+  },
+  {
+    "revision": "46ad6db67c08665d1e0098fd9e10f530",
+    "url": "/road-runners/static/media/foreigner-webfont.46ad6db6.ttf"
+  },
+  {
+    "revision": "ea7fc56f84afc9580bba24074c05bef3",
+    "url": "/road-runners/static/media/foreigner-webfont.ea7fc56f.woff2"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/road-runners/precache-manifest.5cac81272cb02e458b36a46d558ab3ae.js"
+  "/road-runners/precache-manifest.4ba3bcff46c9cde154f65f4657bae139.js"
 );
 
 self.addEventListener('message', (event) => {
